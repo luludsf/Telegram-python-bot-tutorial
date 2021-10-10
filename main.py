a8dfd9f79f8f2d661300f2219725ca3da81ddd66
Mel Brandão <91804467+melbzzz@users.noreply.github.com>
2021-10-10 12:51:56 -0300

    created command bop and fox
    command bop gets the user a random dog image and command fox gets the user a random fox image

--- main.py
+++ main.py
@@ -11,16 +11,12 @@ def test(message):
     bot.send_message(message.chat.id, "Bem vindo ao meu bot")
 
 # Exemple of a command echoing a massgem provided by the user.
-
-
 @bot.message_handler(commands=['echo'])
 def test(message):
     messageText = message.text.replace("/echo", "")
     bot.send_message(message.chat.id, messageText)
 
 # Exemple of a command sending a picture to the user.
-
-
 @bot.message_handler(commands=['foto'])
 def test(message):
     messageText = message.text.replace("/echo", "")
@@ -28,9 +24,22 @@ def test(message):
     photo = open('photo.png', 'rb')
     bot.send_photo(message.chat.id, photo)
 
+# Exemple of a command consuming a api to get a random dog image to the user
+@bot.message_handler(commands=['bop'])
+def bop(message):
+    url = get_image_url('https://random.dog/woof.json','url')
+    chat_id = message.chat.id
+    bot.send_photo(chat_id,url)
     
-# Auxiliary methods 
 
+# Exemple of a command consuming a api to get a random fox image to the user 
+@bot.message_handler(commands=['fox'])
+def fox(message):
+    url = get_image_url('https://some-random-api.ml/img/fox','link')
+    chat_id = message.chat.id
+    bot.send_photo(chat_id,url)
+  
+# Auxiliary methods 
 
 def get_url(url, key):
     contents = requests.get(url).json()
@@ -45,6 +54,4 @@ def get_image_url(url, key):
         url = get_url(url, key)
         file_extension = re.search("([^.]*)$", url).group(1).lower()
     
-    
-
 bot.polling()
