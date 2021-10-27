34e898770533420f6b7b2b812882ec24745d08de
Lucas Moraes <moraessouzalucas@gmail.com>
2021-10-26 21:13:23 -0300

    update: main.py
    add dogs command

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ load_dotenv()
 
 bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
 
-# Exemple of an initial command. (/start)
+# Example of an initial command. (/start)
 @bot.message_handler(commands=['start'])
 def test(message):
     bot.send_message(message.chat.id, "Bem vindo ao meu bot")
 
-# Exemple of a command echoing a message provided by the user.
+# Example of a command echoing a message provided by the user.
 @bot.message_handler(commands=['echo'])
 def test(message):
     messageText = message.text.replace("/echo", "")
     bot.send_message(message.chat.id, messageText)
 
-# Exemple of a command sending a picture to the user.
+# Example of a command sending a picture to the user.
 @bot.message_handler(commands=['foto'])
 def test(message):
     messageText = message.text.replace("/echo", "")
@@ -26,7 +26,7 @@ def test(message):
     photo = open('photo.png', 'rb')
     bot.send_photo(message.chat.id, photo)
 
-# Exemple of a command consuming a api to get a random dog image to the user
+# Example of a command consuming a api to get a random dog image to the user
 @bot.message_handler(commands=['bop'])
 def bop(message):
     url = get_image_url('https://random.dog/woof.json','url')
@@ -34,7 +34,7 @@ def bop(message):
     bot.send_photo(chat_id,url)
     
 
-# Exemple of a command consuming a api to get a random fox image to the user 
+# Example of a command consuming a api to get a random fox image to the user 
 @bot.message_handler(commands=['fox'])
 def fox(message):
     url = get_image_url('https://some-random-api.ml/img/fox','link')
@@ -46,17 +46,22 @@ def fox(message):
 def on_ping(message):
     bot.reply_to(message, "Still alive and kicking!")
     
-# Exemple of a command consuming a api to get a random cat image to the user
+# Example of a command consuming a api to get a random cat image to the user
 @bot.message_handler(commands=['cat'])
 def cat(message):
     url = get_image_url('https://some-random-api.ml/animal/cat', 'image')
     chat_id = message.chat.id
     bot.send_photo(chat_id, url)
-    
+
+# Example of a command consuming a api to get a random dog image to the user (similar to bop but with another api)
+@bot.message_handler(commands=['dog'])
+def dog(message):
+    url = get_image_url('https://some-random-api.ml/animal/dog', 'image')
+    chat_id = message.chat.id
+    bot.send_photo(chat_id, url)
     
 
 # Auxiliary methods 
-
 def get_url(url, key):
     contents = requests.get(url).json()
     url = contents[key]
